35e50b59d6ad0464f7fc689a4b2dd05a2a6570ad
Varya <shchekotovavarya@mail.ru>
2025-05-06 15:47:13 +0300

    #3

---
+++ Movie/app/src/main/java/com/example/movie/Movie.kt
@@ -0,0 +1,12 @@
+package com.example.movie
+
+import androidx.annotation.DrawableRes
+
+data class Movie(
+    val title: String,
+    val age: String,
+    val genre: String,
+    val reviews: Int,
+    val duration: Int,
+    @DrawableRes val posterRes: Int
+)
