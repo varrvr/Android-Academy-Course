733aa4ce1df3c521817372894aaaca8c721f355b
Varya <shchekotovavarya@mail.ru>
2025-05-06 17:47:38 +0300

    #4 basic fragment navigation

--- Movie/app/src/main/java/com/example/movie/MovieListFragment.kt
+++ Movie/app/src/main/java/com/example/movie/MovieListFragment.kt
@@ -5,28 +5,34 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.FrameLayout
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
-
+import com.google.android.material.card.MaterialCardView
 
 class MovieListFragment : Fragment() {
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-
-    }
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        return inflater.inflate(R.layout.fragment_movie_list, container, false)
+    ): View = inflater.inflate(R.layout.fragment_movie_list, container, false)
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val card = view.findViewById<MaterialCardView>(R.id.cardMovie)
+
+        card.setOnClickListener {
+            val details = MovieDetailsFragment().apply {
+                arguments = Bundle().apply {
+                    putString("title", "Avengers: Endgame")
+                    //toDo добавить другие поля
+                }
+            }
+            parentFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, details)
+                .addToBackStack(null)
+                .commit()
+        }
     }
-
-
 }
